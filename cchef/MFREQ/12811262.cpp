ce91bb2169c991dcb9f09147ad4444db9ab5e455
Rahul Bagad <rahul.bagad@walchandsangli.ac.in>
2017-04-21 14:55:57 +0530

    cchef solution for MFREQ with solutionId 12811262 is added.

---
+++ cchef/MFREQ/12811262.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+ 
+int main()
+{
+    long int n,q,i;
+    cin>>n>>q;
+    long int arr[n];
+    for(i=0;i<n;i++)
+        cin>>arr[i];
+    while(q--)
+    {
+        long int l,r,k,flg=0,count=0;
+ 
+        cin>>l>>r>>k;
+        
+        long int cnt=1;count=1;
+        for(i=l;i<=r-1;i++)
+        {
+            if(arr[i]==arr[i-1])
+            {
+                count++;
+                if(count>=k)
+                {
+                    cout<<arr[i]<<endl;flg=1;break;
+                }
+            }
+            else
+            {
+                count=1;
+            }
+        }
+ 
+        if(!flg)
+            cout<<"-1"<<endl;
+    }
+ 
+ 
+    return 0;
+} 
